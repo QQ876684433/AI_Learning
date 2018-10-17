895d36042417b4c942b75c99adffd09d9ac6e3ef
steve <chph13420146901@gmail.com>
2018-10-17 23:09:46 +0800

    房价预测 线性回归

---
+++ AliTianChi/机器学习扫盲/房价预测.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = [1400, 1600, 1700, 1875, 1100, 1550, 2350, 2450, 1425]
+Y = [245000, 312000, 279000, 308000, 199000, 219000, 405000, 324000, 319000]
+
+z1 = np.polyfit(X, Y, 1)
+p1 = np.poly1d(z1)
+
+x = np.arange(1000, 3000)
+y = z1[0] * x + z1[1]
+plt.figure()
+plt.scatter(X, Y)
+plt.plot(x, y)
+plt.show()
+
+print(p1(2000))
